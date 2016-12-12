189691605a1675fd811480e9c501ccf425ab0112
Aleksandr <alext24@gmail.com>
2016-12-13 01:45:10 +0200

    create form of calc1

--- calc1.php
+++ calc1.php
@@ -1,53 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <h1 align = 'center' style = 'color: darkblue'>Калькулятор</h1>
-    <link rel = "stylesheet" href = "form.css" />
-</head>
-
-<style type = "text/CSS">
-    .b1 {
-        background: navy; /* Синий цвет фона */
-        color: white;
-        font-size: 9pt;
-    }
-    .width {
-        width: 500px;
-        height: 30px;
-    }
-    .add {
-        width: 50px;
-        height: 50px;
-        background-color: gainsboro;
-    }
-    .C {
-        width: 50px;
-        height: 50px;
-        background-color: orangered;
-    }
-</style>
-
-<body>
-<form method = "post" action = "calc1.php">
-    <p align = "center">
-        <input type = "text" name = "value" size = "20" class = "width"
-               value="<?php if (isset($_POST["value"])) echo $_POST["value"] ?>" /></p>
-    <p style = "text-indent: 230px"><label>Ans = <?php if (isset($_POST["value"]))
-        echo $_POST["value"] ?></label></p>
-    <p style = "text-indent: 400px">
-        <input type = "submit"  class = "add" name = "openbkt" value = "(" />
-        <input type = "submit"  class = "add" name = "closebkt" value = ")" />
-        <input type = "submit"  class = "add" name = "procent" value = "%" />
-        <input type = "submit"  class = "C" name = "clear" value = "C" />
-
-    </p>
-</form>
-</body>
-</html>
-
-
 <?php
 $errors = [];
+if (isset($_POST['value'])){
+    $value = trim($_POST['value']);
+    if (!preg_match('/^(\-){0,1}[\d]+(\.){0,1}[\d]*$/', $value)){
+        $errors[] = 'Error value type!!!';
+    }
+    else {
+        $action = $_POST['button'];
+        switch ($action) {
+            case 'C':
+                $value = 0;
+                //$_POST['value'] = 0;
+                break;
+            case '/':
+                $value = $value / $_POST['value'];
+                $_POST['value'] /= 100;
+                break;
+            case '7':
+                $value = 7;
+                $_POST['value'] = 7;
+                break;
+            case '8':
+                $value = 8;
+                $_POST['value'] = 8;
+                break;
+            case '9':
+                $value = 9;
+                $_POST['value'] = 9;
+                break;
+        }
+    }
+}
+else{
+    $value = '';
+}
+
+
+if (!empty($errors)) {
+    foreach ($errors as $error) {
+        echo "<div style = 'color: red;'>$error</div>" . "<br />";
+    }
+}
+
 /*if (isset($_POST['firstval']) || ($_POST['secondval']) || ($_POST['operand'])){
     $firstval = trim($_POST['firstval']);
     $secondval = trim($_POST['secondval']);
@@ -107,7 +101,84 @@ switch ($operand){
         }
 }*/
 
+//<p style = "text-indent: 250px">
+
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <h1 align = 'center' style = 'color: darkblue'>Калькулятор</h1>
+</head>
 
+<style type = "text/CSS">
+    .b1 {
+        background: navy; /* Синий цвет фона */
+        color: white;
+        font-size: 9pt;
+    }
+    .width {
+        width: 270px;
+        height: 40px;
+        text-align: end;
+        font-size: larger;
+    }
+    .add {
+        width: 50px;
+        height: 50px;
+        background-color: gainsboro;
+        font-size: larger;
+    }
+    .C {
+        width: 50px;
+        height: 50px;
+        background-color: orangered;
+        font-size: larger;
+    }
+    .ravno {
+        width: 50px;
+        height: 50px;
+        background-color: lightsteelblue;
+        font-size: larger;
+    }
+</style>
 
+<body>
+<form method = "post" action = "calc1.php">
+    <p align = "center">
+        <input type = "text" name = "value" size = "20" class = "width"
+               value = "<?php echo $value ?>" /></p>
+    <p style = "text-indent: 400px"><label>Ans = <?php
+            echo $value ?></label></p>
+    <p align="center">
+        <input type = "submit"  class = "add" name = "button" value = "7" />
+        <input type = "submit"  class = "add" name = "button" value = "8" />
+        <input type = "submit"  class = "add" name = "button" value = "9" />
+        <input type = "submit"  class = "add" name = "button" value = "/" />
+        <input type = "submit"  class = "C" name = "button" value = "C" />
+    </p>
+    <p align="center">
+        <input type = "submit"  class = "add" name = "button" value = "4" />
+        <input type = "submit"  class = "add" name = "button" value = "5" />
+        <input type = "submit"  class = "add" name = "button" value = "6" />
+        <input type = "submit"  class = "add" name = "button" value = "*" />
+        <input type = "submit"  class = "add" name = "button" value = "%" />
+    </p>
+    <p align="center">
+        <input type = "submit"  class = "add" name = "button" value = "1" />
+        <input type = "submit"  class = "add" name = "button" value = "2" />
+        <input type = "submit"  class = "add" name = "button" value = "3" />
+        <input type = "submit"  class = "add" name = "button" value = "-" />
+        <input type = "submit"  class = "add" name = "button" value = "!!!" />
+
+    </p>
+    <p align="center">
+        <input type = "submit"  class = "add" name = "button" value = "+/-" />
+        <input type = "submit"  class = "add" name = "button" value = "0" />
+        <input type = "submit"  class = "add" name = "button" value = "." />
+        <input type = "submit"  class = "add" name = "button" value = "+" />
+        <input type = "submit"  class = "ravno" name = "button" value = "=" />
+    </p>
+</form>
+</body>
+</html>
