4e5d473a46d8b9cc0c0e6315d65bdda1c724b52a
Aleksandr <alext24@gmail.com>
2016-12-12 22:43:05 +0200

    add calc1.php

--- calc1.php
+++ calc1.php
@@ -2,9 +2,11 @@
 /*use BootstrapPHP\BootstrapPHP;
 include 'BootstrapPHP/BootstrapPHP.php';
 BootstrapPHP::register_autoload();
-
 use BootstrapPHP\Button;
 Button::create('Обычная кнопка', '/link/');*/
+
+
+
 
 
 /*echo "<h1 align = 'center' style = 'color: darkblue'>Калькулятор</h1>";
@@ -76,3 +78,60 @@ switch ($operand){
             }
         }
 }*/
+
+/*
+ <input type = "text" class = "form" name = "operand" size = "5" />
+        <input type = "text" name = "secondval" size = "10" /></p>
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <h1 align = 'center' style = 'color: darkblue'>Калькулятор</h1>
+    <link rel = "stylesheet" href = "form.css" />
+</head>
+<body>
+<a href="calc1.php" style="color: black;">AAAAAAAAAAAAA</a><br>
+<form method = "post" action="calc1.php">
+    <p align = "center">
+        <input type = "text" name = "firstval" size = "10" value="<?php if (isset($_GET["login"])) echo $_POST["firstval"] ?>" />
+
+    <p align = "center"> <input type = "submit" name = "Посчитать" />
+    </p>
+</form>
+</body>
+</html>
+
+<?php
+$errors = [];
+if (isset($_POST['firstval'])){// || ($_POST['secondval']) || ($_POST['operand'])){
+$firstval = trim($_POST['firstval']);
+//$secondval = trim($_POST['secondval']);
+//$operand = trim($_POST['operand']);
+if (!preg_match('/^(\-){0,1}[\d]+(\.){0,1}[\d]*$/', $firstval)){
+$errors[] = 'Error firstval value type!!!';
+}
+/*elseif (!preg_match('/^(\-){0,1}[\d]+(\.){0,1}[\d]*$/', $secondval)){
+$errors[] = 'Error secondval value type!!!';
+}
+elseif (!preg_match('/^[+\-\*\/]{1}$/', $operand)){
+$errors[] = 'Error operand value type!!!';
+}*/
+}
+else{
+$firstval = ''; //$secondval = ''; $operand = '';
+}
+if (!empty($errors)) {
+foreach ($errors as $error) {
+echo "<div style = 'color: red;'>$error</div>" . "<br />";
+//echo "<div style = 'color: red;'>$_POST['firstval']</div>";
+}
+}
+
+
+
+
+?>
