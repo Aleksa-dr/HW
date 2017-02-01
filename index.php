6903d753725dc340ff2cb64583e528f4782ab4cc
Aleksandr Teslenko <alext24@gmail.com>
2017-02-01 20:46:41 +0200

    relize class People

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-class People{
+
+class People
+{
     /**
      * Initial property
      * @var string $name
@@ -15,8 +17,41 @@ class People{
      * @var string $sex
      */
     public $sex;
+    /**
+     * Initial property
+     * @var string $answer
+     */
+    public $answer;
 
-    public function inspectionAge(){
+    /**
+     * Initial People constructor.
+     * @param $name
+     * @param $age
+     * @param $sex
+     */
+    public function __construct($name, $age, $sex)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->sex = $sex;
+    }
 
+    /**
+     * Initial method
+     */
+    public function inspectionAge()
+    {
+        if ($this->age < 0 && $this->age > 100) {
+            $this->answer = $this->name . " don`t born or died";
+        }
+        $this->answer = "Your name: " . $this->name . "<br/>"
+            . "You " . $this->age . " years" . "<br/>"
+            . "You are a " . $this->sex;
+        echo $this->answer;
     }
 }
+
+echo "<h3>";
+$enterUserParams = new People('Alex', 24, 'Man');
+$enterUserParams->inspectionAge();
+echo "</h3>";
