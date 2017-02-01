ae97d6d3c63c15b75e4789e70dd54f6992eb9946
Aleksandr Teslenko <alext24@gmail.com>
2017-02-01 18:32:21 +0200

    add class People

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksandr
- * Date: 01.02.17
- * Time: 16:41
- */
+class People{
+    /**
+     * Initial property
+     * @var string $name
+     */
+    public $name;
+    /**
+     * Initial property
+     * @var integer $age
+     */
+    public $age;
+    /**
+     * Initial property
+     * @var string $sex
+     */
+    public $sex;
+
+    public function inspectionAge(){
+
+    }
+}
