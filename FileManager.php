3aff4ab2143b5e5535a4c63189e561597e3f3931
Aleksandr Teslenko <alext24@gmail.com>
2017-02-02 22:49:40 +0200

    creat first version of fileManager

---
+++ FileManager.php
@@ -0,0 +1,47 @@
+<?php include_once 'index.php' ?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset=utf-8">
+    <meta http-equiv="Content-Language" content="ru">
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>FileMenedger</title>
+    </head>
+<body>
+<form method="post" action="FileManager.php">
+    <p align="left">
+    <h3>Укажите путь к файлу :</h3>
+    <input type="text" name="value" class="path"
+           value="<?php echo $filePath ?>"/>
+    </p>
+    <p>
+        <input type="submit" name="button" class="btn"
+               value="Перейти к директории >>>"/>
+    </p>
+    <p>
+    <h3>Файлы данной директории :</h3>
+    <p>
+        <select name="list" class="list">
+            <?php
+            foreach ($listFiles as $key => $value) {
+                echo '<option value=' . $key . '>' . $value . '</option>';
+                echo ' ';
+            } ?>"/>
+        </select>
+        <input type="submit" name="button" class="btnInfo"
+               value="Информация о файле"/>
+    </p>
+    <p><?php echo $nameFile ?></p>
+    <p><?php
+        if (!empty($errors)) {
+            foreach ($errors as $error) {
+                echo "<div style = 'color: red;'>$error</div>"."<br />";
+            }
+        }?></p>
+</form>
+</body>
+</html>
